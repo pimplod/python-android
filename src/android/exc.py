9df4ed8ff1939fec28c908dcfb8bf3acb7db39b7
Chris Soyars <ctsoyars@gmail.com>
2010-07-07 22:37:04 -0400

    Initial AndroidOTA class.

---
+++ src/android/exc.py
@@ -0,0 +1,8 @@
+class AndroidException():
+    pass
+
+class NoFilenameException(AndroidException):
+    pass
+
+class FileNotFoundException(AndroidException):
+    pass
--- src/android/ota.py
+++ src/android/ota.py
@@ -1,5 +1,48 @@
+from android.exc import NoFilenameException
+import zipfile
+
+class FileNotOpenedException():
+    pass
+
 class AndroidOTA(object):
-    def __init__(self, filename):
+    def __init__(self, filename = None):
         self.filename = filename
         self.buildprops = {}
-        sef.zip = zipfile.ZipFile(filename, "r")
+    
+    def open(self, filename = None):
+        if filename:
+            fn = self.filename = filename
+        elif self.filename:
+            fn = self.filename
+        else:
+            raise NoFilenameException()
+        
+        if fn:
+            self.zip = zipfile.ZipFile(fn, "r")
+    
+    def getBuildProp(self, key):
+        if self.buildprop == {} and self.zip:
+            f = self.zip.open("system/build.prop").read()
+            
+            for line in [x.split("=") for x in f.split("\n")]:
+                if len(line) == 2:
+                    self.buildprop[line[0]] = line[1]
+                    
+            value = self.buildprop.get(key, None)
+            if value:
+                return value
+            else:
+                raise KeyError()
+        else:
+            raise FileNotOpenedException()
+        
+    def getModVersion(self):
+        return self.getBuildProp("ro.modversion")
+    
+    def getDevice(self):
+        return self.getBuildProp("ro.product.device")
+
+if __name__ == "__main__":
+    ota = AndroidOTA()
+    ota.open()
+    ota.getModVersion()
