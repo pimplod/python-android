de983f617f4e5ac989fb39c7496c0156a7611c46
Chris Soyars <ctso@ctso.me>
2010-10-01 02:04:10 -0400

    Add license.

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
-__version__ = '1.0.1'
+#!/usr/bin/env python
+#
+# python-android
+# Copyright (C) 2010 Chris Soyars
+#
+# This progream is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the license, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+__version__ = '1.0.2'
 
 from setuptools import setup, find_packages
 
@@ -6,9 +25,16 @@ setup (
        name='python-android',
        version=__version__,
        author='Chris Soyars',
-       author_email='me@ctso.me',
-       description='A collection of tools for interacting with Android builds.',
+       author_email='python@ctso.me',
+       description='A collection of Android tools written in Python.',
        url='http://github.com/ctso/python-android',
+       license="GNU GPL",
+       classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Topic :: Software Development :: Libraries',
+        ],
 
        packages=find_packages('src'),
        package_dir={'':'src'},
--- src/android/boot.py
+++ src/android/boot.py
@@ -1,3 +1,19 @@
+# python-android
+# Copyright (C) 2010 Chris Soyars
+#
+# This progream is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the license, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import struct
 from binascii import hexlify
 
@@ -6,12 +22,12 @@ class BootImage(object):
         self.filename = filename
         self._fd = open(filename, "r")
         self._header = self._fd.read(584)
-        
+
         self._loadInfo()
-        
+
     def _loadInfo(self):
         header = struct.unpack("<8s8I8s16s512s8s", self._header)
-        
+
         self.magic = header[0]
         self.kernel_size = header[1]
         self.kernel_addr = header[2] - 0x00008000
@@ -25,19 +41,19 @@ class BootImage(object):
         self.name = header[10]
         self.cmdline = header[11]
         self.checksum = hexlify(header[12]).upper()
-        
+
     def getKernelBase(self):
         return str("%#x" % self.kernel_addr)
-    
+
     def getKernelSize(self):
         return self.kernel_size
-    
+
     def getRamdiskSize(self):
         return self.ramdisk_size
-    
+
     def getRamdiskBase(self):
         return str("%#x" % self.ramdisk_addr)
-    
+
     def dump(self):
         print "============================================="
         print "Information about %s" % self.filename
@@ -47,7 +63,7 @@ class BootImage(object):
         print "Ramdisk Size : %s" % self.getRamdiskSize()
         print "Ramdisk Base : %s" % self.getRamdiskBase()
         print "============================================="
-    
+
 if __name__ == '__main__':
     bi = BootImage()
     bi.dump()
